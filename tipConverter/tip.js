558d086f3c12ae117d30ef3f2f4ecac7d4b82766
Nicolaas0 <nicolerrick@gmail.com>
2021-01-18 21:49:31 +0700

    creating calculation function

--- tipConverter/tip.js
+++ tipConverter/tip.js
@@ -1,3 +1,38 @@
-var amount = document.getElementById('inputam').value;
-var service = document.getElementByid('servS').value;
-var person = document.getElementById('inputpers').value;
+function calculation(){
+
+    var amount = document.getElementById('ab').value;
+    var service = document.getElementById('as').value;
+    var person = document.getElementById('np').value;
+
+    if (amount === "" || service == 0) {
+
+        alert("Please input the value");
+
+        return;
+    }
+
+    if (person === "" || person <= 1) {
+
+        person = 1;
+
+        document.getElementById('each').style.display="none";
+
+    }else{
+
+        document.getElementById('each').style.display="inline-block";
+
+    }
+
+    var total = (amount * service) / person;
+
+
+    document.getElementById('rs').style.display="block";
+
+    document.getElementById('total').innerHTML =total;
+    
+}
+
+
+document.getElementById('btn').onclick = function(){
+    calculation();
+};
