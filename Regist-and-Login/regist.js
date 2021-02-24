8b779cd97867956079ecd8dfe35591448613661e
nicolaas <nicolerrick@gmail.com>
2021-02-24 18:03:11 +0700

    done validating empty inputs

--- Regist-and-Login/regist.js
+++ Regist-and-Login/regist.js
@@ -1,1 +1,18 @@
+var btn = document.getElementById('btn');
 
+function validation() {
+
+  var x = document.getElementsByTagName('INPUT');
+
+  for (var i = 0; i < x.length; i++){
+    var value = x[i].value;
+
+    if (value !== null && value !== "") {
+      x[i].style.borderColor = "#e9ff70";
+    } else if (value == null || value == "") {
+      x[i].style.borderColor = "#70d6ff";
+    }
+  };
+}
+
+btn.addEventListener("click", validation);
