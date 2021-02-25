c7c55a3b89f336954f979571b60405d4eb982283
nicolaas <nicolerrick@gmail.com>
2021-02-25 23:01:55 +0700

    add code for transition

--- Regist-and-Login/regist.js
+++ Regist-and-Login/regist.js
@@ -1,24 +1,23 @@
-const BTN = document.getElementById('btn');
-const STAT = document.getElementById('stat');
-var input = document.getElementsByTagName('input');
+const BTN = document.getElementById("btn");
+const STAT = document.getElementById("stat");
+var input = document.getElementsByTagName("input");
 function empval() {
-
   // var x = document.getElementsByTagName('INPUT');
 
-  for (var i = 0; i < input.length; i++){
+  for (var i = 0; i < input.length; i++) {
     var value = input[i].value;
 
     if (value !== null && value !== "") {
-    input[i].style.borderColor = "#e9ff70";
+      input[i].style.borderColor = "#e9ff70";
+      input[i].style.transition = "all 0.7s";
     } else if (value == null || value == "") {
       input[i].style.borderColor = "#70d6ff";
+      input[i].style.transition = "all 1s";
       STAT.innerHTML = "Highlighted input cannot be empty.";
     }
 
-    input[i].addEventListener('focusout', empval);
-  };
+    input[i].addEventListener("focusout", empval);
+  }
 }
-
-
 
 BTN.addEventListener("click", empval);
