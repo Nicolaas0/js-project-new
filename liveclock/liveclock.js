1063971b159d19cd09df319c3d275422aead0531
nicolaas <nicolerrick@gmail.com>
2021-02-28 20:44:37 +0700

    unfinished js

--- liveclock/liveclock.js
+++ liveclock/liveclock.js
@@ -1,19 +1,33 @@
-function clock(){
-    var date = new Date();
-    var h = date.getHours();
-    var m = date.getMinutes();
-    var s = date.getSeconds();
+const BTN = document.getElementById("chg");
 
-    m = time(m);
-    s = time(s);
+function clock() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
 
-    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
-    var t = setTimeout(clock, 500);
+  m = time(m);
+  s = time(s);
+
+  document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+  var t = setTimeout(clock, 500);
 }
 
-function time(i){
-    if (i < 10) {i = "0"+ i};
-    return i;
-
-
+function time(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
+
+function image() {
+  var arr = ["assest/dogmemes.jpg", "assest/meme2-min.jpg"];
+
+  for (let i = 0; i < arr.length; i++) {
+    var img = document.getElementById("img");
+
+    img.src = arr[i];
+  }
+}
+
+BTN.addEventListener("click", image);
