a563cd47763c1d873e6b1c51f4b49cd47bf967c6
nicolaas <nicolerrick@gmail.com>
2021-03-01 21:26:43 +0700

    prettier for formating code

--- Regist-and-Login/regist.js
+++ Regist-and-Login/regist.js
@@ -1,23 +1,22 @@
 const BTN = document.getElementById("btn");
 const STAT = document.getElementById("stat");
 var input = document.getElementsByTagName("input");
-function empval() {
+
   // var x = document.getElementsByTagName('INPUT');
+  function empval(){
+    for (var i = 0; i < input.length; i++) {
+      var value = input[i].value;
 
-  for (var i = 0; i < input.length; i++) {
-    var value = input[i].value;
-
-    if (value !== null && value !== "") {
-      input[i].style.borderColor = "#e9ff70";
-      input[i].style.transition = "all 0.7s";
-    } else if (value == null || value == "") {
-      input[i].style.borderColor = "#70d6ff";
-      input[i].style.transition = "all 1s";
-      STAT.innerHTML = "Highlighted input cannot be empty.";
+      if (value !== null && value !== "") {
+        input[i].style.borderColor = "#e9ff70";
+        input[i].style.transition = "all 0.7s";
+      } else if (value == null || value == "") {
+        input[i].style.borderColor = "#70d6ff";
+        input[i].style.transition = "all 1s";
+        STAT.innerHTML = "Highlighted input cannot be empty.";
+      }
+      input[i].addEventListener("focusout", empval);
     }
-
-    input[i].addEventListener("focusout", empval);
-  }
 }
 
 BTN.addEventListener("click", empval);
