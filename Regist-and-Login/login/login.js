b7f8f8b017bc1dd5db33057bd2375b60274b0ea4
nicolaas <nicolerrick@gmail.com>
2021-03-27 00:01:10 +0700

    add the basic js

---
+++ Regist-and-Login/login/login.js
@@ -0,0 +1,19 @@
+const UNAME = document.getElementById('uname');
+const PASS = document.getElementById('pass');
+const BTN = document.getElementById('btn');
+
+function validate() {
+    var username = UNAME.value;
+    var password = PASS.value;
+
+    var warning = document.getElementById('warn');
+
+    if (username || password !== null && username || password !== "") {
+        warning.innerHTML = "";
+    } else {
+        warning.innerHTML = "Please fill the empty inputs";
+    }
+}
+
+BTN.addEventListener('click', validate);
+
