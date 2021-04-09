57a29288cf30bce0b70ad4ba9873a0bfd2453469
Nicolaas0 <nicolerrick@gmail.com>
2021-04-09 23:49:38 +0700

    remake the js

--- Regist-and-Login/login/login.js
+++ Regist-and-Login/login/login.js
@@ -1,19 +1,2 @@
-const UNAME = document.getElementById('uname');
-const PASS = document.getElementById('pass');
-const BTN = document.getElementById('btn');
 
-function validate() {
-    var username = UNAME.value;
-    var password = PASS.value;
-
-    var warning = document.getElementById('warn');
-
-    if (username || password !== null && username || password !== "") {
-        warning.innerHTML = "Login Successfully!";
-    } else {
-        warning.innerHTML = "Please fill the empty inputs";
-    }
-}
-
-BTN.addEventListener('click', validate);
 
